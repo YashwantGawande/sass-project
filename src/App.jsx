704e446b9c9fd3d895c9532d00a6a6f6d865dabc
YashwantNG <gawandeyashwant7@gmail.com>
2023-03-08 23:47:48 +0530

    Add Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Footer from "./components/footer/Footer";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import "./App.scss"
+
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
